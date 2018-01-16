d890fba34c7e596db7d183c628c4e3786dcd1d69
gnoyixiang <gnoyixiang@gmail.com>
2018-01-16 12:36:13 +0800

    included entities

---
+++ InventoryWebApp/Models/Entities/Department.cs
@@ -0,0 +1,60 @@
+namespace InventoryWebApp.Models.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Department")]
+    public partial class Department
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Department()
+        {
+            Disbursements = new HashSet<Disbursement>();
+            Employees = new HashSet<Employee>();
+            Requests = new HashSet<Request>();
+        }
+
+        [Key]
+        [StringLength(20)]
+        public string DepartmentCode { get; set; }
+
+        [StringLength(100)]
+        public string DepartmentName { get; set; }
+
+        [StringLength(20)]
+        public string CollectionPointCode { get; set; }
+
+        [StringLength(50)]
+        public string Contact_Name { get; set; }
+
+        [StringLength(50)]
+        public string TelephoneNo { get; set; }
+
+        [StringLength(50)]
+        public string FaxNo { get; set; }
+
+        [StringLength(20)]
+        public string HeadCode { get; set; }
+
+        [StringLength(20)]
+        public string RepresentativeCode { get; set; }
+
+        public virtual CollectionPoint CollectionPoint { get; set; }
+
+        public virtual Employee Employee { get; set; }
+
+        public virtual Employee Employee1 { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Disbursement> Disbursements { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Employee> Employees { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Request> Requests { get; set; }
+    }
+}
---
+++ InventoryWebApp/Models/Entities/User.cs
@@ -0,0 +1,49 @@
+namespace InventoryWebApp.Models.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("User")]
+    public partial class User
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public User()
+        {
+            UserClaims = new HashSet<UserClaim>();
+            UserLogins = new HashSet<UserLogin>();
+            Roles = new HashSet<Role>();
+        }
+
+        public string Id { get; set; }
+
+        [StringLength(256)]
+        public string Email { get; set; }
+
+        [StringLength(512)]
+        public string PasswordHash { get; set; }
+
+        [StringLength(512)]
+        public string SecurityStamp { get; set; }
+
+        [StringLength(128)]
+        public string PhoneNumber { get; set; }
+
+        public int AccessFailedCount { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        public string UserName { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<UserClaim> UserClaims { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<UserLogin> UserLogins { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Role> Roles { get; set; }
+    }
+}
