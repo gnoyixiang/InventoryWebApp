a111d4dbb2b44a3e88a9f0346232e01cb3fbcc25
JY <JY@DESKTOP-N1DGA2I>
2018-01-16 12:04:37 +0800

    create request dao

---
+++ InventoryWebApp/DAO/RequestDAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventoryWebApp.Models;
+
+namespace InventoryWebApp.DAO
+{
+    interface RequestDAO
+    {
+        List<Request> findAllRequests();
+        Request findRequest(string RequestCode);
+        Request createRequest(Request Req);
+        Request changeRequest(Request Req);
+        void removeRequest(Request Req);
+        Request findRequestByID(string RequestCode);
+
+    }
+}
