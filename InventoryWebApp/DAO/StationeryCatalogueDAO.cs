7f23ca77e61dcd308a095c6082809be1bba200a5
phamquynhtrang3011 <you@example.com>
2018-01-16 23:26:47 +0800

    cm

--- InventoryWebApp/DAO/StationeryCatalogueDAO.cs
+++ InventoryWebApp/DAO/StationeryCatalogueDAO.cs
@@ -33,15 +33,15 @@ namespace InventoryWebApp.DAO
         }
         public int AddStationery(StationeryCatalogue st)
         {
-            //try
-            //{
+            try
+            {
                 em.StationeryCatalogues.Add(st);
-                return em.SaveChanges(); 
-            //}
-            //catch (Exception e)
-            //{
-            //    return 0;
-            //}
+                return em.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
         }
         public int UpdateStationery(StationeryCatalogue st)
         {
