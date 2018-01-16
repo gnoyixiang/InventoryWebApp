abaa7f2a07ed0d84968f5ce902d1e7aa921e1af2
gnoyixiang <gnoyixiang@gmail.com>
2018-01-16 12:06:13 +0800

    commit some

--- InventoryWebApp/DAO/PurchaseOrderDAO.cs
+++ InventoryWebApp/DAO/PurchaseOrderDAO.cs
@@ -5,9 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace InventoryWebApp.DAO
-{
+{ 
     interface PurchaseOrderDAO
     {
-        
+            
     }
 }
--- InventoryWebApp/DAO/PurchaseOrderDAOImpl.cs
+++ InventoryWebApp/DAO/PurchaseOrderDAOImpl.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using InventoryWebApp.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace InventoryWebApp.DAO
 {
-    public class PurchaseOrderDAOImpl:PurchaseOrderDAO
+    class PurchaseOrderDAOImpl:PurchaseOrderDAO
     {
+        EntityModel em;
+        List<StationeryCatalogue> getAllStationeries()
+        {
+            using (em = new EntityModel())
+            {
+                return em.StationeryCatalogues.ToList<StationeryCatalogue>();
+            }
+        }
+
+        StationeryCatalogue getStationeriesByItemCode(string itemCode)
+        {
+            using (em = new EntityModel())
+            {
+                return em.StationeryCatalogues.Where(s => s.ItemCode == itemCode).FirstOrDefault<StationeryCatalogue>();
+            }
+        }
+
+
     }
 }
