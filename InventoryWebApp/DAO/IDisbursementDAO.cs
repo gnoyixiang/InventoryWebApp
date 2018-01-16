0e3f7990d7d819163fe132f36de6ffe2415fdf04
gnoyixiang <gnoyixiang@gmail.com>
2018-01-16 18:53:58 +0800

    cleanup and patch

--- InventoryWebApp/DAO/IDisbursementDAO.cs
+++ InventoryWebApp/DAO/IDisbursementDAO.cs
@@ -15,6 +15,7 @@ namespace InventoryWebApp.DAO
         List<Disbursement> SearchDbmByPeriodCreated(DateTime date1, DateTime date2);
         List<Disbursement> SearchDbmByPeriodDisbursed(DateTime date1, DateTime date2);
         List<Disbursement> SearchDbmByStatus(string status);
+        List<Disbursement> SearchDisbursementByCode(string disbursementCode);
         int UpdateDbmDisbursementDetail(Disbursement d);
         int UpdateDbmStatus(Disbursement d);
         int UpdateDisbursement(Disbursement d);
