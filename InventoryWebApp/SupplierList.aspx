315fc47178bc7416d17a81eba0fe378da5fade30
Amit <amitmohanty56>
2018-01-17 15:32:32 +0800

    changed nothing

--- InventoryWebApp/SupplierList.aspx
+++ InventoryWebApp/SupplierList.aspx
@@ -2,8 +2,6 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
-
     <asp:GridView ID="gvSupplierList" runat="server"></asp:GridView>
-
 
 </asp:Content>
--- InventoryWebApp/SupplierList.aspx.cs
+++ InventoryWebApp/SupplierList.aspx.cs
@@ -5,19 +5,13 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-
 namespace InventoryWebApp
 {
     public partial class SupplierList : System.Web.UI.Page
     {
-        
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-                gvSupplierList.DataSource =
-                gvSupplierList.DataBind();
-            }
+
         }
     }
 }
