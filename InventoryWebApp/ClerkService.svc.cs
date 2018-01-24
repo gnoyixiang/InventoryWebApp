98cee2b9054811bfecf6f7acfd92a7fbe8cd4bc1
Nathalie <nathalie.nhu.y@gmail.com>
2018-01-24 14:42:12 +0800

    commit

--- InventoryWebApp/ClerkService.svc.cs
+++ InventoryWebApp/ClerkService.svc.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
-using System.ServiceModel.Web;
+using System.ServiceModel;
 using System.Text;
-
 
 namespace InventoryWebApp.WCF
 {
