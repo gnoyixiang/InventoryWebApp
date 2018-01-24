63f304af6c60b79fbc6bb3d144964a76c5669d73
Nathalie <nathalie.nhu.y@gmail.com>
2018-01-24 14:20:26 +0800

    commit

--- InventoryWebApp/DAO/DisbursementDetailsDAO.cs
+++ InventoryWebApp/DAO/DisbursementDetailsDAO.cs
@@ -47,18 +47,21 @@ namespace InventoryWebApp.DAO
 
         public List<DisbursementDetail> ListDDByItemCode(string ItemCode,DateTime start)
         {
-            em = new EntityModel();
-            List<DisbursementDetail> ddList = new List<DisbursementDetail>();
-            List<DisbursementDetail> ddListAfterStartDate = new List<DisbursementDetail>();
-            // List<Disbursement> dList = em.Disbursements.Where(dd => dd.DateDisbursed >= start).ToList();
-            ddList =em.DisbursementDetails.Where(dd => dd.ItemCode == ItemCode).ToList();
-            foreach (DisbursementDetail d in ddList) {
-                if( em.Disbursements.Where(x=>x.DisbursementCode==d.DisbursementCode && x.DateDisbursed >= start).FirstOrDefault() != null)
+            using (EntityModel em = new EntityModel())
+            {
+                List<DisbursementDetail> ddList = new List<DisbursementDetail>();
+                List<DisbursementDetail> ddListAfterStartDate = new List<DisbursementDetail>();
+                // List<Disbursement> dList = em.Disbursements.Where(dd => dd.DateDisbursed >= start).ToList();
+                ddList = em.DisbursementDetails.Where(dd => dd.ItemCode == ItemCode).ToList();
+                foreach (DisbursementDetail d in ddList)
                 {
-                    ddListAfterStartDate.Add(d);
+                    if (em.Disbursements.Where(x => x.DisbursementCode == d.DisbursementCode && x.DateDisbursed >= start).FirstOrDefault() != null)
+                    {
+                        ddListAfterStartDate.Add(d);
+                    }
                 }
+                return ddListAfterStartDate;
             }
-            return ddListAfterStartDate;
         }
     }
 }
