5a990f44033aee567a577bbb5113ab7c8e909287
Nathalie <nathalie.nhu.y@gmail.com>
2018-02-02 11:52:51 +0800

    commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -16,9 +16,8 @@ namespace ConsoleTestApp
         public static void Main(String[] args)
         {
             IClerkService cs = new ClerkService();
-            WCF_CollectionDate wcd = new WCF_CollectionDate();
-            wcd.DatePlanToCollect = "11 Jul 2018";
-            cs.ConfirmCollectionDate("08 Feb 2018", "nathalie@ssis.edu.sg", "null");
+            cs.GetOutstandingRequests();
+            
         }   
      }
 }
